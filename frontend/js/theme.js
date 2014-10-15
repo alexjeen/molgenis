40a45815be94525acc200416301a6240241b1b56
Alex Jeensma (Mac Pro) <alex@vontis.nl>
2014-10-15 15:36:02 +0200

    view dataset en home zo goed als af

--- frontend/js/theme.js
+++ frontend/js/theme.js
@@ -10,6 +10,7 @@ $(function(){
 			$(this).find('> a').removeClass('open');			
 		}
 	);
+	// custom selects ed
 	$('form select').selectpicker({
 		style: 'btn-primary'
 	});	
@@ -17,4 +18,21 @@ $(function(){
 		checkboxClass: 'icheckbox_square-blue',
 		radioClass: 'iradio_square-blue'
 	});	
+	// collapsable panels
+	$('.panel.collapsable').each(function(){
+		$(this).find('.panel-title').prepend('<i class="collapsePanel fa fa-caret-up"></i>');
+		
+	});
+	$('.collapsable .panel-heading').click(function(){
+		var $pb = $(this).parents('.collapsable').find('.panel-body');
+		$pb.slideToggle('slow', function(){
+			if($(this).is(':visible')) {
+				$(this).parents('.collapsable').find('.collapsePanel').addClass('fa-caret-up').removeClass('fa-caret-down');
+			}
+			else {
+				$(this).parents('.collapsable').find('.collapsePanel').removeClass('fa-caret-up').addClass('fa-caret-down');				
+			}
+		});
+		return false;
+	});
 });
